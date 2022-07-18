4f52b95e32eb88eee52573e3ba1e70b08e9b33c8
Himanshu Vaidya <hnic07@gmail.com>
2022-07-18 09:57:18 -0400

    commit tree 52f8a951327afe0f5acb92d224293cdc713a8165

--- methods.rb
+++ methods.rb
@@ -1,1 +1,16 @@
-# Your code here!
+def greet_programmer 
+    puts "Hello, programmer!"
+end
+
+def greet (name = "programmer")
+    puts "Hello, #{name}!"
+end
+
+greet "Jimmy"
+greet "Naureen"
+
+def greet_with_default
+    if name != "Naureen"
+        return name
+end
+    greet name = "programmer"
